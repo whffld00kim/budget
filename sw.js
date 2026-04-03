c2015bd7aa058ac38298165bf3b5e0661ad289f2
whffld00kim <whffld00.kim@gmail.com>
2026-04-04 04:28:01 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'gaegybu-v20260404';
+const ASSETS = ['./', './index.html', './css/style.css', './js/app.js', './manifest.json'];
+
+// 설치: 핵심 파일 캐시
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+// 활성화: 이전 캐시 삭제
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 요청: 네트워크 우선, 실패 시 캐시 사용
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
