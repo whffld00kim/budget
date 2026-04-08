eeb1f3ed17bcc80f6a9db977fab669c3b7e17ce7
whffld00kim <whffld00.kim@gmail.com>
2026-04-09 05:25:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gaegybu-v20260409';
+const CACHE = 'gaegybu-v20260411';
 const ASSETS = ['./', './index.html', './css/style.css', './js/app.js', './manifest.json'];
 
 // 설치: 핵심 파일 캐시
@@ -7,14 +7,15 @@ self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-// 활성화: 이전 캐시 삭제
+// 활성화: 이전 캐시 삭제 후 모든 클라이언트에 새로고침 요청
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(client => client.postMessage({ type: 'SW_UPDATED' })))
   );
-  self.clients.claim();
 });
 
 // 요청: 네트워크 우선, 실패 시 캐시 사용
